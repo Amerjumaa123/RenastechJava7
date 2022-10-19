f69b9eaf3e8bf3ce069719117da318d8b9d8e3b1
Amer <Amer.jumaa540@gmail.com>
2022-10-19 19:19:28 -0400

    new class

---
+++ src/Day10_StringMethods/HomeWork.java
@@ -0,0 +1,10 @@
+package Day10_StringMethods;
+
+import java.util.Scanner;
+
+public class HomeWork {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+    }
+}
